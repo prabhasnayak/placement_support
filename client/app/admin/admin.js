ed4b921dee1e8524bbbead7c28fa842657ce19f0
prabhas125 <prabhas.nayak@gmail.com>
2014-09-02 12:06:04 +0530

    Initial codebase

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('collegeApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
